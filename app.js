7f56cb2ba22a21f16758c3f998bf8c4970aba442
Omri <omri.katz@kaltura.com>
2015-02-17 23:31:47 +0200

    Features added
    Sending messages from popup window to the injection script is now
    supported.

--- app.js
+++ app.js
@@ -11,13 +11,28 @@
 
 var myApp = angular.module('myApp', []);
 
+
+function hello() {
+  chrome.tabs.executeScript({
+    file: '/inject/inject.js'
+  }); 
+}
+ 
+var e = document.getElementById('jiraButton');
+console.log(e);
+
+
 myApp.controller('customerCareCtrl', function ($scope) {
     Foundation.global.namespace = '';
     $scope.activeFoundation = $(document).foundation();
 	$scope.openJira = function() {
+        chrome.tabs.executeScript({}, function() {
+            console.log("hello world");
+        }); 
 		if (typeof $scope.jira != 'undefined') {
 			var url = 'https://kaltura.atlassian.net/browse/' + $scope.jira;
-			window.open(url, '_blank');
+			// window.open(url, '_blank');
+
 		}
 	}
     $scope.openSalesforce = function() {
@@ -27,6 +42,13 @@ myApp.controller('customerCareCtrl', function ($scope) {
         }
     }
 });
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+            console.log(response.farewell);
+        });
+    });
+
 
 myApp.directive('myEnter', function () { //directive that listen to "Enter" keypress
     return function (scope, element, attrs) {
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -23,6 +23,17 @@ $(document).ready(function() {
 		}
 	}
 
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(sender.tab ?
+			"from a content script:" + sender.tab.url :
+			"from the extension");
+		if (request.greeting == "hello")
+			sendResponse({farewell: "goodbye"});
+});
+
+
+
 
 	var script = document.createElement('script');
 	script.appendChild(document.createTextNode('('+ main +')();'));
