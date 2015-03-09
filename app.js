076e9a5a57159160fcfd0a8e9296f0462085f1bc
omri <omrikatz12@gmail.com>
2015-03-09 15:17:08 +0200

    Added Automatic get KS when clicking popup icon

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ myApp.controller('customerCareCtrl', function ($scope) {
 		}
 	}
 
+
     $scope.openSalesforce = function() {
         if (typeof $scope.salesforce != 'undefined') {
             var url = 'https://na5.salesforce.com/_ui/search/ui/UnifiedSearchResults?searchType=2&sen=a1d&sen=a0S&sen=00a&sen=005&sen=001&sen=500&sen=003&str=' + $scope.salesforce;
@@ -104,3 +105,11 @@ myApp.directive('myEnter', function () { //directive that listen to "Enter" keyp
         });
     };
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var scope = angular.element($("body")).scope();
+    scope.this.getKs();
+});
+
