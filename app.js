e900bac4d03c64a0fa531b540c47937159438ee4
Omri Katz <omri.katz@kaltura.com>
2015-08-19 15:55:02 +0300

    moved to material angular

--- app.js
+++ app.js
@@ -9,14 +9,17 @@
  * http://corp.kaltura.com
  */
 
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['ngMaterial']).config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('orange');
+});
 
 chrome.contextMenus.create({
   id: 'open',
-  title: chrome.i18n.getMessage('openContextMenuTitle'),
+  title: 'test',
   contexts: ['link'],
 });
-
 
 myApp.controller('customerCareCtrl', function ($scope) {
 
@@ -24,59 +27,55 @@ myApp.controller('customerCareCtrl', function ($scope) {
     $scope.activeFoundation = $(document).foundation();
     $(function () {
         $('[data-toggle="popover"]').popover()
-    })
-    $scope.getKs = function() {
+    });
 
-        //send message from browser_action(popup window) to content_script(inject.js) to display prompt with the ks.
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {action: "getKs"}, function(response) {
-                $scope.$apply(function() {
-                    if (response)
-                         $scope.ks = response.ks["kmc.vars.ks"];
+    $scope.popUp = {
+
+        getKs: function() {
+            var that = this;
+            //send message from browser_action(popup window) to content_script(inject.js) to display prompt with the ks.
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {action: "getKs"}, function(response) {
+                    $scope.$apply(function() {
+                        if (response)
+                            that.ks = response.ks["kmc.vars.ks"];
                         // $("#joyrideDiv").foundation('joyride', 'start');
-                        
+
+                    });
+                    $('#ks-input-box').select();
+                    document.execCommand('copy');
+
                 });
-                $('#ks-input-box').select();
-                document.execCommand('copy');
+            });
+        },
+        openSalesforce: function() {
+            if (typeof this.salesForce !== 'undefined') {
+                var url = 'https://na5.salesforce.com/_ui/search/ui/UnifiedSearchResults?searchType=2&sen=a1d&sen=a0S&sen=00a&sen=005&sen=001&sen=500&sen=003&str=' + this.salesForce;
+                window.open(url, '_blank');
+            }
+        },
+        openJira: function() {
+            if (typeof this.jira !== 'undefined') {
+                var url = 'https://kaltura.atlassian.net/browse/' + this.jira;
+                window.open(url, '_blank');
 
-            });  
-        });
-    }
-
-    $scope.getPlayerInfo = function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-           chrome.tabs.sendMessage(tabs[0].id, {action: "getPlayerInfo"}, function(response) {});  
-        });
-    }
-
-    $scope.displayURLQRCode = function($event) {
-        document.getElementById('spinner').style.display = 'block';
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            }
+        },
+        displayURLQRCode: function() {
+            document.getElementById('spinner').style.display = 'block';
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
                 document.getElementById('spinner').style.display = 'none';
                 $scope.qrCodeImgSrc = 'http://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=H&chl=' + encodeURIComponent(tabs[0].url);
                 $scope.$apply();
-        });
-    }
-
-
-	$scope.openJira = function() {
-
-		if (typeof $scope.jira !== 'undefined') {
-			var url = 'https://kaltura.atlassian.net/browse/' + $scope.jira;
-			window.open(url, '_blank');
-
-		}
-	}
-
-
-    $scope.openSalesforce = function() {
-        if (typeof $scope.salesforce !== 'undefined') {
-            var url = 'https://na5.salesforce.com/_ui/search/ui/UnifiedSearchResults?searchType=2&sen=a1d&sen=a0S&sen=00a&sen=005&sen=001&sen=500&sen=003&str=' + $scope.salesforce;
-            window.open(url, '_blank');
+            });
+        },
+        getPlayerInfo: function() {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {action: "getPlayerInfo"}, function(response) {});
+            });
         }
     }
 });
-
 
 
 
@@ -99,6 +98,6 @@ myApp.directive('myEnter', function () { //directive that listen to "Enter" keyp
 
 document.addEventListener('DOMContentLoaded', function() {
     var scope = angular.element($("body")).scope();
-    scope.this.getKs();
+    scope.popUp.getKs();
 });
 
